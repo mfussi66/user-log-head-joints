f1f52507fe28279913643e0a826ffe99a2443f97
Mattia Fussi <mattia.fussi@iit.it>
2023-01-30 14:50:46 +0100

    convert to rfmodule

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include <yarp/os/PeriodicThread.h>
 #include <yarp/os/Property.h>
 #include <yarp/os/ResourceFinder.h>
+#include <yarp/os/RFModule.h>
 #include <yarp/os/Time.h>
 
 #include <cstdlib>
@@ -23,48 +24,45 @@
 
  /******************************************************************************/
 struct DataExperiment {
-  double t;
   double pwm;
   double pid_out;
   double enc;
   double ref;
   double err;
   double target;
+  double taskspace_enc;
 };
 
 constexpr int PITCH_JOINT_ID = 0;
 constexpr int ROLL_JOINT_ID = 1;
+constexpr int YAW_JOINT_ID = 2;
+constexpr int CAM_JOINT_ID = 3;
 
-bool TERMINATED = false;
-
-/******************************************************************************/
-// The function to be called when ctrl-c (SIGINT) is sent to process
-void signal_callback_handler(int signum) {
-  // Terminate program
-  TERMINATED = true;
-}
-
-class ReadThread : public yarp::os::PeriodicThread {
+class ReadThread : public yarp::os::RFModule {
 private:
   yarp::dev::PolyDriver driver;
   yarp::dev::IEncoders* iEnc{ nullptr };
   yarp::dev::IPidControl* iPidCtrl{ nullptr };
   yarp::dev::IPositionControl* iPosCtrl{ nullptr };
-  std::ofstream roll_fout, pitch_fout;
+  std::ofstream fout, pitch_fout;
   std::string remote;
+  std::string fname;
   std::vector<DataExperiment> yaw_data;
   std::vector<DataExperiment> roll_data;
   std::vector<DataExperiment> pitch_data;
+  std::vector<DataExperiment> cam_data;
+  std::vector<double> time_vector;
+  double period;
   double t0;
 
 public:
-  ReadThread(yarp::conf::float64_t period, std::string remote_port)
-    : PeriodicThread(period) {
-    remote = remote_port;
-  }
 
-  bool threadInit() {
+  bool configure(yarp::os::ResourceFinder &rf) {
     yInfo() << "ReadThread starting";
+
+    auto remote = rf.check("remote", yarp::os::Value("/ergocub/head")).asString();
+    auto roll_filename = rf.check("file", yarp::os::Value("ecub_head.csv")).asString();  // log roll data
+    period = rf.check("period", yarp::os::Value(0.03)).asFloat64();
 
     yarp::os::Property conf;
     conf.put("device", "remote_controlboard");
@@ -94,86 +92,106 @@ public:
       return false;
     }
 
-    roll_fout.open("logfile.log");
+    fout.open(roll_filename);
 
-    if (!roll_fout.is_open()) {
-      yError() << "Failed to open "
-        << "logfile.log";
+    if (!fout.is_open()) {
+      yError() << "Failed to open " << roll_filename;
       driver.close();
       return false;
     }
 
-    roll_fout << "Time"<< ","<< "Mot1"<< ","<< "Mot2"<< ","<< "Mot3"<< ","
-      << "Mot1_Ref"<< ","<< "Mot2_Ref"<< ","<< "Mot3_Ref"<< ","
-      << "Mot1_Err"<< ","<< "Mot2_Err"<< ","<< "Mot3_Err" << std::endl;
-
+    fout << "Time"<< ","
+      << "Mot0_Pwm"<< ","<< "Mot1_Pwm"<< ","<< "Mot2_Pwm" << ","<< "Mot3_Pwm" << ","
+      << "Mot0_Ref"<< ","<< "Mot1_Ref"<< ","<< "Mot2_Ref" << ","<< "Mot3_Ref" << ","
+      << "Mot0_Err"<< ","<< "Mot1_Err"<< ","<< "Mot2_Err" << ","<< "Mot3_Err" << ","
+      << "Roll,Pitch,Yaw,Cam" << std::endl;
     t0 = yarp::os::Time::now();
+
+    yInfo() << "Started recording...";
 
     return true;
   }
 
-  void run() {
+  double getPeriod() {
+    return period;
+  }
+
+  bool updateModule() {
     DataExperiment yaw_d;
     DataExperiment roll_d;
     DataExperiment pitch_d;
-    auto delta_t = yarp::os::Time::now() - t0;
-    roll_d.t = pitch_d.t = delta_t;
-    yaw_d.t = delta_t;
+    DataExperiment cam_d;
+    time_vector.push_back(static_cast<double>(yarp::os::Time::now() - t0));
 
-    iEnc->getEncoder(0, &yaw_d.enc);
-    iEnc->getEncoder(1, &roll_d.enc);
-    iEnc->getEncoder(2, &pitch_d.enc);
+    iEnc->getEncoder(YAW_JOINT_ID, &yaw_d.taskspace_enc);
+    iEnc->getEncoder(ROLL_JOINT_ID, &roll_d.taskspace_enc);
+    iEnc->getEncoder(PITCH_JOINT_ID, &pitch_d.taskspace_enc);
+    iEnc->getEncoder(CAM_JOINT_ID, &cam_d.taskspace_enc);
 
-    iPidCtrl->getPidReference(yarp::dev::VOCAB_PIDTYPE_POSITION, 0,
+    iPidCtrl->getPidReference(yarp::dev::VOCAB_PIDTYPE_POSITION, YAW_JOINT_ID,
       &yaw_d.ref);
-    iPidCtrl->getPidReference(yarp::dev::VOCAB_PIDTYPE_POSITION, 1,
+    iPidCtrl->getPidReference(yarp::dev::VOCAB_PIDTYPE_POSITION, ROLL_JOINT_ID,
       &roll_d.ref);
-    iPidCtrl->getPidReference(yarp::dev::VOCAB_PIDTYPE_POSITION, 2,
+    iPidCtrl->getPidReference(yarp::dev::VOCAB_PIDTYPE_POSITION, PITCH_JOINT_ID,
       &pitch_d.ref);
+    iPidCtrl->getPidReference(yarp::dev::VOCAB_PIDTYPE_POSITION, CAM_JOINT_ID,
+      &cam_d.ref);
 
-    if (!iPosCtrl->getTargetPosition(0, &yaw_d.target))
+    if (!iPosCtrl->getTargetPosition(YAW_JOINT_ID, &yaw_d.target))
       yWarning() << "Cannot retrieve yaw tg";
-    if (!iPosCtrl->getTargetPosition(1, &roll_d.target))
+    if (!iPosCtrl->getTargetPosition(ROLL_JOINT_ID, &roll_d.target))
       yWarning() << "Cannot retrieve roll tg";
-    if (!iPosCtrl->getTargetPosition(2, &pitch_d.target))
+    if (!iPosCtrl->getTargetPosition(PITCH_JOINT_ID, &pitch_d.target))
       yWarning() << "Cannot retrieve pitch tg";
+    if (!iPosCtrl->getTargetPosition(CAM_JOINT_ID, &cam_d.target))
+      yWarning() << "Cannot retrieve cam tg";
 
-    iPidCtrl->getPidError(yarp::dev::VOCAB_PIDTYPE_POSITION, 0, &yaw_d.err);
-    iPidCtrl->getPidError(yarp::dev::VOCAB_PIDTYPE_POSITION, 1, &roll_d.err);
-    iPidCtrl->getPidError(yarp::dev::VOCAB_PIDTYPE_POSITION, 2, &pitch_d.err);
+    iPidCtrl->getPidError(yarp::dev::VOCAB_PIDTYPE_POSITION, YAW_JOINT_ID, &yaw_d.err);
+    iPidCtrl->getPidError(yarp::dev::VOCAB_PIDTYPE_POSITION, ROLL_JOINT_ID, &roll_d.err);
+    iPidCtrl->getPidError(yarp::dev::VOCAB_PIDTYPE_POSITION, PITCH_JOINT_ID, &pitch_d.err);
+    iPidCtrl->getPidError(yarp::dev::VOCAB_PIDTYPE_POSITION, CAM_JOINT_ID, &cam_d.err);
 
-    iPidCtrl->getPidOutput(yarp::dev::VOCAB_PIDTYPE_POSITION, 0, &yaw_d.pwm);
-    iPidCtrl->getPidOutput(yarp::dev::VOCAB_PIDTYPE_POSITION, 1, &roll_d.pwm);
-    iPidCtrl->getPidOutput(yarp::dev::VOCAB_PIDTYPE_POSITION, 2, &pitch_d.pwm);
+    iPidCtrl->getPidOutput(yarp::dev::VOCAB_PIDTYPE_POSITION, YAW_JOINT_ID, &yaw_d.pwm);
+    iPidCtrl->getPidOutput(yarp::dev::VOCAB_PIDTYPE_POSITION, ROLL_JOINT_ID, &roll_d.pwm);
+    iPidCtrl->getPidOutput(yarp::dev::VOCAB_PIDTYPE_POSITION, PITCH_JOINT_ID, &pitch_d.pwm);
 
     yaw_data.push_back(std::move(yaw_d));
     roll_data.push_back(std::move(roll_d));
     pitch_data.push_back(std::move(pitch_d));
+    cam_data.push_back(std::move(cam_d));
+
+    yInfoThrottle(10) << "Recording data...";
+
+    return true;
   }
 
-  void threadRelease() {
-    for (uint32_t i = 0; i < roll_data.size(); ++i) {
-      roll_fout << yaw_data[i].t << "," << yaw_data[i].pwm << ","
-        << roll_data[i].pwm << "," << pitch_data[i].pwm << ","
-        << yaw_data[i].ref << "," << roll_data[i].ref << ","
-        << pitch_data[i].ref << "," << yaw_data[i].err << ","
-        << roll_data[i].err << "," << pitch_data[i].err << std::endl;
+  bool close() {
+
+    yInfo() << "Catched SIGINT, saving data to file...";
+
+
+    for (size_t i = 0; i < roll_data.size(); ++i) {
+      fout << time_vector[i] << ","
+        << yaw_data[i].pwm << "," << roll_data[i].pwm << "," << pitch_data[i].pwm << "," << cam_data[i].pwm << ","
+        << yaw_data[i].ref << "," << roll_data[i].ref << "," << pitch_data[i].ref << "," << cam_data[i].ref << "," 
+        << yaw_data[i].err << "," << roll_data[i].err << "," << pitch_data[i].err << "," << cam_data[i].err << "," 
+        << roll_data[i].taskspace_enc << "," << pitch_data[i].taskspace_enc << ","  << yaw_data[i].taskspace_enc << "," << cam_data[i].taskspace_enc 
+        << std::endl;
     }
-    yInfo() << "Saved data";
 
     // Close I/O resources
-    roll_fout.close();
+    fout.close();
     // pitch_fout.close();
     driver.close();
 
     yInfo() << "Done.";
+
+    return true;
   }
 };
 
-/******************************************************************************/
+/****************************************************************q`**************/
 int main(int argc, char* argv[]) {
-  // Register signal and signal handler
-  signal(SIGINT, signal_callback_handler);
 
   yarp::os::Network yarp;
   if (!yarp.checkNetwork()) {
@@ -182,27 +200,11 @@ int main(int argc, char* argv[]) {
   }
 
   yarp::os::ResourceFinder rf;
+  rf.setDefaultContext("userloghead");
+  rf.setDefaultConfigFile("config.ini");
   rf.configure(argc, argv);
 
-  auto remote = rf.check("remote", yarp::os::Value("/nwa/wrist_mc")).asString();
-  auto roll_filename = rf.check("filename", yarp::os::Value("yaw_cmd.log"))
-    .asString();  // log roll data
-  auto period = rf.check("period", yarp::os::Value(0.01)).asFloat64();
+  ReadThread read_thread;
 
-  ReadThread read_thread(period, remote);
-
-  yInfo() << "Started recording...";
-  read_thread.start();
-
-  while (!TERMINATED) {
-    yInfo() << "Recording data...";
-    yarp::os::Time::delay(5);
-  }
-
-  if (TERMINATED) {
-    yInfo() << "Catched SIGINT, saving data to file...";
-    read_thread.stop();
-  }
-
-  return EXIT_SUCCESS;
+  return read_thread.runModule(rf);
 }
